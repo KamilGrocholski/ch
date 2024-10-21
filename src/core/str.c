f1b77e25ce6b7a3f5aaf2119874a2c0c310ec4a6
Kamil Grocholski <mikololo26@gmail.com>
2024-10-21 20:40:24 +0200

    feat(core/str): Add more str utils

--- src/core/str.c
+++ src/core/str.c
@@ -48,6 +48,30 @@ str_t str_cut_suffix(str_t str, str_t suffix) {
     };
 }
 
+str_t str_pop_first_split(str_t* str, str_t split_by) {
+    i32 index = str_index(*str, split_by);
+    if (index == -1) {
+        str_t out = *str;
+        *str = STR_EMPTY;
+        return out;
+    }
+    str_t out = str_substr(*str, 0, index + 1);
+    *str = str_substr(*str, index + split_by.length, str->length); 
+    return out;
+}
+
+str_t str_pop_first_split_char(str_t* str, char split_by) {
+    i32 index = str_index_char(*str, split_by);
+    if (index == -1) {
+        str_t out = *str;
+        *str = STR_EMPTY;
+        return out;
+    }
+    str_t out = str_substr(*str, 0, index + 1);
+    *str = str_substr(*str, index + 1, str->length); 
+    return out;
+}
+
 i32 str_index(str_t str, str_t substr) {
     if (str.length < substr.length) {
         return -1;
@@ -67,6 +91,26 @@ i32 str_index_char(str_t str, char ch) {
         }
     }
     return -1;
+}
+
+b8 str_cut(str_t str, str_t separator, str_t* before, str_t* after) {
+    i32 index = str_index(str, separator);
+    if (index == -1) {
+        return false;
+    }
+    *before = (str_t){ .length = index + 1, .data = str.data };
+    *after = (str_t){ .length = str.length - separator.length - index, .data = str.data + separator.length + index };
+    return true;
+}
+
+b8 str_cut_char(str_t str, char separator, str_t* before, str_t* after) {
+    i32 index = str_index_char(str, separator);
+    if (index == -1) {
+        return false;
+    }
+    *before = (str_t){ .length = index + 1, .data = str.data };
+    *after = (str_t){ .length = str.length - 1 - index, .data = str.data + 1 + index };
+    return true;
 }
 
 b8 str_is_empty(str_t str) {
@@ -89,6 +133,10 @@ b8 str_compare(str_t a, str_t b) {
     return true;
 }
 
+b8 str_compare_char(str_t a, char ch) {
+    return a.length == 1 && a.data[0] == ch;
+}
+
 b8 str_contains(str_t str, str_t substr) {
     return str_index(str, substr) >= 0;
 }
@@ -101,12 +149,30 @@ b8 str_has_prefix(str_t str, str_t prefix) {
     if (str.length < prefix.length) {
         return false;
     }
-    return strncmp(str.data, prefix.data, prefix.length);
+    return strncmp(str.data, prefix.data, prefix.length) == 0;
+}
+
+b8 str_has_prefix_char(str_t str, char prefix) {
+    return str.data == prefix;
 }
 
 b8 str_has_suffix(str_t str, str_t suffix) {
     if (str.length < suffix.length) {
         return false;
     }
-    return strncmp(str.data + str.length - suffix.length, suffix.data, suffix.length);
+    return strncmp(str.data + str.length - suffix.length, suffix.data, suffix.length) == 0;
+}
+
+b8 str_has_suffix_char(str_t str, char prefix) {
+    return str.data[str.length - 1] == prefix;
+}
+
+u32 str_count_char(str_t str, char ch) {
+    u32 n = 0;
+    for (u32 i = 0; i < str.length; i++) {
+        if (str.data[i] == ch) {
+            n++;
+        }
+    }
+    return n;
 }
--- src/core/str.h
+++ src/core/str.h
@@ -23,9 +23,17 @@ str_t str_cut_prefix(str_t str, str_t prefix);
 
 str_t str_cut_suffix(str_t str, str_t suffix);
 
+str_t str_pop_first_split(str_t* str, str_t split_by);
+
+str_t str_pop_first_split_char(str_t* str, char split_by);
+
 i32 str_index(str_t str, str_t substr);
 
 i32 str_index_char(str_t str, char ch);
+
+b8 str_cut(str_t str, str_t separator, str_t* before, str_t* after);
+
+b8 str_cut_char(str_t str, char separator, str_t* before, str_t* after);
 
 b8 str_is_empty(str_t str);
 
@@ -33,10 +41,18 @@ b8 str_is_null(str_t str);
 
 b8 str_compare(str_t a, str_t b);
 
+b8 str_compare_char(str_t a, char ch);
+
 b8 str_contains(str_t str, str_t substr);
 
 b8 str_contains_char(str_t str, char ch);
 
 b8 str_has_prefix(str_t str, str_t prefix);
 
-b8 str_has_suffix(str_t str, str_t prefix);
+b8 str_has_prefix_char(str_t str, char prefix);
+
+b8 str_has_suffix(str_t str, str_t suffix);
+
+b8 str_has_suffix_char(str_t str, char suffix);
+
+u32 str_count_char(str_t str, char ch);
